c94c8a7db636a5b608859fdb83b75df8b28ad8b4
unknown <danielleokia1@gmail.com>
2023-02-01 18:33:54 +0300

    added home routes

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -5,18 +5,29 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
-    const commentData = await findAll({
-      attributes
+    const blogData = await Blog.findAll({
+      attributes: [
+      'id',
+      'post_name',
+      'description',
+      'created_at'
+      ],
       include:[
-      {
+        {
+          model: Comment,  
+          attributes: ['id', 'comment', 'blog_id', 'user_id', 'created_at'],
+        },
+      
+        {
         model: User,  
-        exclude: ["password"],
+        attributes: ['name'],
       },
+
     ]
   });
-    const comments = commentData.map(comment => comment.get({plain:true}))
+    const blogs = blogData.map(blog => blog.get({plain:true}));
 
-    res.render('homepage', {comments});
+    res.render('homepage', {blogs});
   } catch (err) {
     res.status(500).json(err);
   }
@@ -30,6 +41,45 @@ router.get('/login', async (req, res) => {
   }
 });
 
+router.get('/:id', (req, res) => {
+  Blog.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'post_name', 'description', 'created_at'],
+    include: [
+      {
+        model: User,
+        attributes: ['name']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment', 'blog_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['name']
+        }
+      }
+    ]
+  })
+    .then(blogData => {
+      if (!blogData) {
+        res.status(404).json({ message: 'No blog found with this id' });
+        return;
+      }
+      res.json(blogData);
+    })
+    const blog = blogData.get({plain:true});
+    res.render('single-blog', {blog,
+      logged_in: req.session.logged_in
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 
+
+    
 module.exports = router;
