81d61de8b6d16b647a07709ac4c7ecfd9710df80
unknown <danielleokia1@gmail.com>
2023-02-09 17:22:39 -0800

    added js and handlebars code for new blog and single blog

--- Main/public/js/comment-submit.js
+++ Main/public/js/comment-submit.js
@@ -1,17 +1,32 @@
-const commentSubmitInput = document.querySelector("#comment-submit");
-const submitBtn = document.querySelector("#comment-submit");
 
 
-function submitComment(event) {
+async function submitComment(event) {
+event.preventDefault();
+    
+    const comment = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const blog_id = window.location.toString().split('/')[
+      window.location.toString().split('/'). length - 1
+    ];
 
-    const inputVal = {
-    input: commentSubmitInput.value
+if (comment) {
+   const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+         blog_id,
+         comment
+       }),
+      headers: { 'Content-Type': 'application/json'
     }
+});
 
-    fetch("/api/newcomment", {
-        method: 'POST',
-      body: JSON.stringify({ inputVal }),
-      headers: { 'Content-Type': 'application/json' },
-    })
+if (response.ok) {
+  document.location.reload();
+} else {
+  alert(response.statusText);
 }
-submitBtn.addEventListener("click", submitComment)
+}
+}
+
+
+
+document.querySelector('.comment-form').addEventListener('submit', submitComment);
---
+++ Main/public/js/new-blog.js
@@ -0,0 +1,30 @@
+
+
+async function submitBlog(event) {
+event.preventDefault();
+    
+    const blog_title = document.querySelector('textarea[name="blog-title"]').value;
+    const blog_text = document.querySelector('textarea[name="blog-text"]').value;
+
+
+   const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+         blog_title,
+         blog_text
+       }),
+      headers: { 'Content-Type': 'application/json'
+      }
+});
+
+if (response.ok) {
+  document.location.replace('/dashboard');
+} else {
+  alert(response.statusText);
+}
+}
+
+
+
+
+document.querySelector('.new-blog-form').addEventListener('submit', submitBlog)
